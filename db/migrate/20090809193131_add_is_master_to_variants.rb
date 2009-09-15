1b517c760093399b0baa70264a62958d1a87b256
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2009-09-15 08:40:41 +0200

    Fixed 2 problems with migrations
    one with calculators not loading when upgrading,
    and migration converting to new master variants running in O(n^2)
    (few minutes for large datasets)
    
    [#738]

--- db/migrate/20090809193131_add_is_master_to_variants.rb
+++ db/migrate/20090809193131_add_is_master_to_variants.rb
@@ -14,9 +14,10 @@ class AddIsMasterToVariants < ActiveRecord::Migration
     variants = Variant.all
     unless variants.empty? 
       # select first variant of each product as "master" and flag it in the database
-      sorted_variants = variants.sort{|a,b| a.product_id <=> b.product_id}.sort{|a,b| a.id <=> b.id}
-      master_variants = sorted_variants.inject([]) {|m, v| m << v unless m.detect{|d| d.product_id == v.product_id}; m}
-      master_variants.each{|v| v.update_attributes(:is_master => true, :price => v.product.attributes["master_price"])}
+      master_variants = ActiveSupport::OrderedHash.new
+      sorted_variants = variants.reject{|v| v.product_id.nil?}.sort{|a,b| a.product_id <=> b.product_id}.sort{|a,b| a.id <=> b.id}
+      sorted_variants.each{|v| master_variants[v.product] ||= v }
+      master_variants.each_pair{|product, v| v.update_attributes(:is_master => true, :price => product.attributes["master_price"])}
     end
   end
 
