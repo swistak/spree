a1b2636db6daa94d9622db80a0a1f7ab0689ad3e
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2009-10-21 15:34:53 +0200

    Charge refactoring
    This commit introduces a lot of changes, all should be backwards
    compatible, except for change of behaviour of charge recalculating for
    completed orders.
    
    - Separate TaxCharge and ShippingCharge back into separate classes
      inheriting from common Charge < Adjustment
    - Separate CouponCredit nto separate class and inherit from Credit <
      Adjustment as a step for implementing other credit types
    - Introduce #applicable? method for Adjustment to determine if charge is
      still applicable for order.
    - After amount for adjustment is saved(during Order#complete), it's no
      longer recalculated, so it can be adjusted by hand
    - updated fixtures and factories to produce new types of charges
    - introduced test to check new adjustment functionality
    - removed 'secondary_type' column since it's no longer needed
    
    This changes are more in line with my initial charges design, they solve
    problem of charges beeing recalculated after order is completed, should
    be much easier to extend (by simple sublclasing of either Charge
    Adjustment or Credit), previous approach required overriding of
    parent class when adding new charge/credit type.
    
    [#841]
    [#765]
    [#647]

--- app/models/adjustment.rb
+++ app/models/adjustment.rb
@@ -6,30 +6,46 @@ class Adjustment < ActiveRecord::Base
 
   validates_presence_of :amount
   validates_presence_of :description
-  validates_numericality_of :amount
+  validates_numericality_of :amount, :allow_nil => true
 
-  before_save do |record|
-    record.secondary_type ||= record.type
-  end
-
+  # Tries to calculate the adjustment, returns nil if adjustment could not be calculated.
+  # raises RuntimeError if adjustment source didn't provide the caculator.
   def calculate_adjustment
     if adjustment_source
-      calc = adjustment_source.calculator || adjustment_source.default_calculator
+      calc = adjustment_source.calculator
+      calc ||= adjustment_source.default_calculator if adjustment_source.respond_to?(:default_calculator)
       raise(RuntimeError, "#{self.class.name}##{id} doesn't have a calculator") unless calc
       calc.compute(adjustment_source)
-    elsif read_attribute(:amount)
-      read_attribute(:amount)
-    else
-      nil
     end
   end
 
+  # Checks if adjustment is applicable for the order.
+  # Should return _true_ if adjustment should be preserved and _false_ if removed.
+  # Default behaviour is to preserve adjustment if amount is present and non 0.
+  # Might (and should) be overriden in descendant classes, to provide adjustment specific behaviour.
+  def applicable?
+    amount && amount != 0
+  end
+
+  # Retrives amount of adjustment, if order hasn't been completed and amount is not set tries to calculate new amount.
   def amount
-    read_attribute(:amount) || self.calculate_adjustment
+    amnt = read_attribute(:amount)
+    if order && order.checkout_complete
+      amnt
+    else
+      amnt == 0 ? (self.calculate_adjustment || 0) : amnt
+    end
   end
   
   def update_amount
     new_amount = calculate_adjustment
     update_attribute(:amount, new_amount) if new_amount
+  end
+
+  def secondary_type; type; end
+
+  private
+  def ensure_negative_amount
+    self.amount = -1 * self.amount.abs if self.amount
   end
 end
