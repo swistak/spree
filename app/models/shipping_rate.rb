40b3e20c8facc82f7a125853f365870964e80689
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2009-10-22 09:24:55 +0200

    name for shipping rate

--- app/models/shipping_rate.rb
+++ app/models/shipping_rate.rb
@@ -3,4 +3,8 @@ class ShippingRate < ActiveRecord::Base
   belongs_to :shipping_category
   
   has_calculator
+
+  def name
+    "#{shipping_method && shipping_method.name} - #{shipping_category && shipping_category.name}"
+  end
 end
