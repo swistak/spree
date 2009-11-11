19393b11acf26e632acdff0067819a9f36ab7ca1
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2009-11-11 22:19:48 +0100

    Improve performance of migration to now scheme.
    Reduces complexity from Product.count * Variant.count queries, to 3
    queries, that run for total of few seconds even on huge amounts of data
    sets.
    
    [#712 state:reopened]

--- db/migrate/20090923100315_add_count_on_hand_to_variants_and_products.rb
+++ db/migrate/20090923100315_add_count_on_hand_to_variants_and_products.rb
@@ -3,18 +3,37 @@ class AddCountOnHandToVariantsAndProducts < ActiveRecord::Migration
     add_column :variants, :count_on_hand, :integer, :default => 0, :null => false
     add_column :products, :count_on_hand, :integer, :default => 0, :null => false
     say_with_time 'Transfering inventory units with status on_hand to variants table...' do 
-      Variant.all.each do |v|
-        v.update_attribute(:count_on_hand, v.inventory_units.with_state("on_hand").size)
-        InventoryUnit.destroy_all(:variant_id => v.id, :state => "on_hand")
-      end
+      execute %q{
+        UPDATE 
+          variants 
+        SET 
+          variants.count_on_hand = (
+            SELECT 
+              COUNT(inventory_units.id) 
+            FROM 
+              inventory_units
+            WHERE 
+              inventory_units.variant_id = variants.id AND 
+              state = "on_hand"
+          );
+      }
+
+      InventoryUnit.destroy_all(:state => "on_hand")
     end
     say_with_time 'Updating products count on hand' do
-      Product.all.each do |p|
-        product_count_on_hand = p.has_variants? ?
-            p.variants.inject(0) {|acc, v| acc + v.count_on_hand} :
-            (p.master ? p.master.count_on_hand : 0)
-        p.update_attribute(:count_on_hand, product_count_on_hand)
-      end
+      execute %q{
+        UPDATE
+          products
+        SET
+          products.count_on_hand = (
+            SELECT 
+              SUM(variants.count_on_hand) 
+            FROM 
+              variants 
+            WHERE 
+              variants.product_id = products.id
+          )
+      }
     end
   end
 
