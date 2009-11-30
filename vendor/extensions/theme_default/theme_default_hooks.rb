955778e2bbfc7b012e736928f621b7121acf0c02
David North <davidnorth@gmail.com>
2009-11-30 20:26:03 +0000

    first pass add adding hooks to public views. document in HOOKS.markdown

--- vendor/extensions/theme_default/theme_default_hooks.rb
+++ vendor/extensions/theme_default/theme_default_hooks.rb
@@ -1,9 +1,14 @@
 class ThemeDefaultHooks < Spree::ThemeSupport::Hook::ViewListener
 
-  #render_on :homepage_above_products, :partial => "shared/test_hook" 
+  # TODO: could we define these automatically by looking in app/views/hooks for partial views
+  render_on :homepage_above_products, :partial => "shared/test_hook" 
   
-  # def homepage_above_products(context)
-  #   'hey there!'
-  # end
+  #def homepage_above_products(context)
+  #   '<p>Above products</p>'
+  #end
+  #
+  #def homepage_below_products(context)
+  #   '<p>Below products</p>'
+  #end
 
 end
