cbc2252abc5c6c89f514db54877b8a46de5f5d48
David North <davidnorth@gmail.com>
2009-12-02 19:58:17 +0000

    fix call_hook helper removed call_hook from application_helper

--- lib/spree/theme_support.rb
+++ lib/spree/theme_support.rb
@@ -1,2 +1,6 @@
-require 'spree/theme_support/hook'
+require_dependency 'spree/theme_support/hook'
 require 'spree/theme_support/more_patches'
+
+ActionController::Base.class_eval do
+  include Spree::ThemeSupport::HookHelper
+end
