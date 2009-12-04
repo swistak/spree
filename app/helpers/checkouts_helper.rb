1fbc364d623e9c4d1dc0f52098bd298bea9b9837
David North <davidnorth@gmail.com>
2009-12-04 21:10:33 +0000

    modify helper that creates checkout progress indicator to correspond with css requirements

--- app/helpers/checkouts_helper.rb
+++ app/helpers/checkouts_helper.rb
@@ -10,17 +10,35 @@ module CheckoutsHelper
   def checkout_progress
     steps = Checkout.state_names.map do |state|
       text = t("checkout_steps.#{state}")
-      if Checkout.state_names.index(@checkout.state) > Checkout.state_names.index(state)
-        css_class = 'completed'
+
+      css_classes = []
+      
+      
+      current_index = Checkout.state_names.index(@checkout.state)
+      state_index = Checkout.state_names.index(state)
+
+      if state_index < current_index
+        css_classes <<'completed'
         text = link_to text, edit_order_checkout_url(@order, :step => state)
-      elsif state == @checkout.state
-        css_class = 'current'
-      else
-        css_class = nil
       end
-      content_tag('li', content_tag('span', text), :class => css_class)
+      if state_index == current_index + 1
+        css_classes << 'next'
+      end
+      if state == @checkout.state
+        css_classes << 'current'
+      end
+
+      if state_index == 0
+        css_classes << 'first'
+      end
+      if state_index == Checkout.state_names.length - 1
+        css_classes << 'last'
+      end
+
+      # It'd be nice to have separate classes but combining them with a dash helps out for IE6 which only sees the last class
+      content_tag('li', content_tag('span', text), :class => css_classes.join('-'))
     end
-    content_tag('ol', steps.join("\n"), :class => 'progress-steps', :id => "checkout-step-#{@checkout.state}")
+    content_tag('ol', steps.join("\n"), :class => 'progress-steps', :id => "checkout-step-#{@checkout.state}") + '<br clear="left" />'
   end
   
 end
